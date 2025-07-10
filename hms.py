25bbe284331b7a9b8fcea22becd9ceeef9a284bb
Oat77 <omarotaylor@outlook.com>
2025-07-10 02:16:30 -0500

    Created calendar for the Doctor class.

--- hms.py
+++ hms.py
@@ -29,38 +29,56 @@ class Patient (Person):
 
 class Doctor (Person):
 
-    doctor_calendar= {}
-
-    months= ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October','November', 'December']
-
-    for month in months:
-        doctor_calendar[month]= []
-
-    for month, days in doctor_calendar.items(): 
-        if month in ('April', 'June' , 'September' , 'November') :
-            for i in range (30):
-                days.append(False)
-        else:
-            for i in range (31):
-                days.append(False)
-
     doctor_id_counter= 70001
+
+    def __init__(self, name, age, gender):
+        super().__init__(name, age, gender)
+        self.get_doctor_id= self.generate_doctor_id()
+        self.doctor_calendar= self.create_calendar()
+ 
+    def create_calendar(self):
+        self.doctor_calendar= {}
+        self.months= ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October','November', 'December']
+        self.doctor_times=[]
+        self.clock= [9,10,11,12,1,2,3,4]
+
+        for month in self.months:
+            self.doctor_calendar[month]= []
+
+        for month, days in self.doctor_calendar.items(): 
+            if month in ('April', 'June' , 'September' , 'November') :
+                for i in range (30):
+                    days.append(False)
+                    self.doctor_times.append(self.clock)
+            else:
+                for i in range (31):
+                    days.append(False)
+                    self.doctor_times.append(self.clock)
+        
+        return self.doctor_calendar
+
+    def check_availability(cls):
+        cls.availabletimes= []
+        cls.check_month= input("Enter month: ")
+        cls.check_day= input("Enter day: ")
+        for cls.hrs in cls.doctor_times[cls.doctor_calendar[cls.check_month][int(cls.check_day)-1]]:
+            if cls.hrs != False:
+                cls.availabletimes.append(cls.hrs)
+        return cls.availabletimes
 
     def generate_doctor_id(cls):
         cls.doctor_id_counter += 1
         return cls.doctor_id_counter
-    
-    def __init__(self, name, age, gender):
-        super().__init__(name, age, gender)
-        self.get_doctor_id= self.generate_doctor_id()
     
     def view_profile(self):
         print("Name:", self.name)
         print("Age:", self.age)
         print("Gender:", self.gender)
         print("Patient ID:", self.get_doctor_id)
+        print("Available Times:", self.check_availability())
 
 
-patient1= Patient("Omaro", 22, 'M')
+doctor1= Doctor("Omaro", 22, 'M')
+doctor1.view_profile()
+doctor1.check_availability
 
-patient1.view_profile()
